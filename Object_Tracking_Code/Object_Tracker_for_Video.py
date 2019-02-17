cf6375e94a280ea952729d1d29e177850b8f9c9e
Edward Preble <epreble@rti.org>
2019-02-17 16:27:57 -0500

    Object tracking code for video files

---
+++ Object_Tracking_Code/Object_Tracker_for_Video.py
@@ -0,0 +1,98 @@
+# Modified version of https://www.learnopencv.com/object-tracking-using-opencv-cpp-python/
+# Install cv2 in python3 via pip install opencv-contrib-python
+# the "contrib" part expands the install to include all the object tracking packages
+
+import cv2
+import sys
+
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+if __name__ == '__main__':
+
+    # Set up tracker. Choose type from list.
+
+    tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
+    tracker_type = tracker_types[0]
+
+    if int(minor_ver) < 3:
+        tracker = cv2.Tracker_create(tracker_type)
+    else:
+        if tracker_type == 'BOOSTING':
+            tracker = cv2.TrackerBoosting_create()
+        if tracker_type == 'MIL':
+            tracker = cv2.TrackerMIL_create()
+        if tracker_type == 'KCF':
+            tracker = cv2.TrackerKCF_create()
+        if tracker_type == 'TLD':
+            tracker = cv2.TrackerTLD_create()
+        if tracker_type == 'MEDIANFLOW':
+            tracker = cv2.TrackerMedianFlow_create()
+        if tracker_type == 'GOTURN':
+            tracker = cv2.TrackerGOTURN_create()
+
+    # Read video
+    # video = cv2.VideoCapture("/Users/epreble/Documents/Data/Youtube/PalmDrone.mp4")
+    # video = cv2.VideoCapture("/Users/epreble/Documents/Data/Raw/Drone_SIF_2017/RTI_Campus_Mosquito_Training/DJI_0006_100ft_white.m4v")
+    video = cv2.VideoCapture("/Users/epreble/Desktop/chaplin.mov")
+    # video = cv2.VideoCapture("/Users/epreble/Desktop/grass_with_trap_small.mp4")
+
+    # Exit if video not opened.
+    if not video.isOpened():
+        print("Could not open video")
+        sys.exit()
+
+    # Read first frame.
+    ok, frame = video.read()
+    if not ok:
+        print('Cannot read video file')
+        sys.exit()
+
+    # Define an initial bounding box
+    # bbox = (287, 23, 86, 320)
+    # bbox = (1393, 1485, 1443, 1535)
+
+    # Uncomment the line below to select a different bounding box
+    bbox = cv2.selectROI(frame, False)
+
+    # Initialize tracker with first frame and bounding box
+    ok = tracker.init(frame, bbox)
+
+    while True:
+        # Read a new frame
+        ok, frame = video.read()
+        if not ok:
+            break
+
+        # Start timer
+        timer = cv2.getTickCount()
+
+        # Update tracker
+        ok, bbox = tracker.update(frame)
+
+        # Calculate Frames per second (FPS)
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+
+        # Draw bounding box
+        if ok:
+            # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+        else:
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+
+        # print(frame)
+
+        # Display tracker type on frame
+        cv2.putText(frame, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
+
+        # Display FPS on frame
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
+
+        # Display result
+        cv2.imshow("Tracking", frame)
+
+        # Exit if ESC pressed
+        k = cv2.waitKey(1) & 0xff
+        if k == 27: break
