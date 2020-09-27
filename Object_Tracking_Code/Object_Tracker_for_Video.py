b68705564917f282885676a35f1e20fdb18b866c
Preble <epreble@RTI-107458.rti.ns>
2020-09-27 17:18:10 -0400

    Add box coords to display

--- Object_Tracking_Code/Object_Tracker_for_Video.py
+++ Object_Tracking_Code/Object_Tracker_for_Video.py
@@ -78,6 +78,8 @@ if __name__ == '__main__':
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
             cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+            # Display Box Coords on frame
+            cv2.putText(frame, "Coords : " + str(int(bbox[0])) + "," + str(int(bbox[1])) + " - " + str(int(bbox[0] + bbox[2])) + "," + str(int(bbox[1] + bbox[3])), (300, 45), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
         else:
             # Tracking failure
             cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
@@ -88,7 +90,7 @@ if __name__ == '__main__':
         cv2.putText(frame, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
 
         # Display FPS on frame
-        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 45), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
 
         # Display result
         cv2.imshow("Tracking", frame)
